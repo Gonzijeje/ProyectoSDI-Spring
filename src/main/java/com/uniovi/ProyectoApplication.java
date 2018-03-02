138c2741e0a523b558e47bf0254df433c80e9328
Gonzalo Collada Vázquez <gonzadjango@gmail.com>
2018-03-02 11:44:53 +0100

    Prueba

--- src/main/java/com/uniovi/ProyectoApplication.java
+++ src/main/java/com/uniovi/ProyectoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProyectoApplication {
+	//
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoApplication.class, args);
